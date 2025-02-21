173a0ed46d05abded2477bd6858f3b85b4bd44cd
Maryam Rahat <maryamrahat0@gmail.com>
2025-02-21 17:32:07 +0530

    fst

--- main.py
+++ main.py
@@ -4,10 +4,8 @@ from typing import List
 
 app = FastAPI()
 
-
 class InputData(BaseModel):
     data: List[str]
-
 
 def process_data(data):
     numbers = [item for item in data if item.isdigit()]
@@ -16,14 +14,13 @@ def process_data(data):
 
     return numbers, alphabets, highest_alphabet
 
-
 @app.post("/bfhl")
 async def process_request(request_data: InputData):
     numbers, alphabets, highest_alphabet = process_data(request_data.data)
 
     response = {
         "is_success": True,
-        "user_id": "saheb_ansari_27072005",  
+        "user_id": "saheb_ansari_27072005",
         "email": "2237451.it.cec@cgc.edu.in",
         "roll_number": "2237451",
         "numbers": numbers,
@@ -31,7 +28,6 @@ async def process_request(request_data: InputData):
         "highest_alphabet": highest_alphabet
     }
     return response
-
 
 @app.get("/bfhl")
 async def get_operation_code():
